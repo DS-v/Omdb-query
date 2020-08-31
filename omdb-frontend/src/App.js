7c3b522a3418e3c46f7e48029ad8fd4316d9ea5b
DS-v <divyanshushuklagamma#gmail.com>
2020-08-31 11:12:13 +0530

    First commit

---
+++ omdb-frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <p className="pa">"Hello World!"</p>
+  );
+}
+
+export default App;
